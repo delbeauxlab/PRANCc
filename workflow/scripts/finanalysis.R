3d82a57a0ac562574912d6908b6426119212c258
cloudy <avcloudy@gmail.com>
2023-09-13 22:07:28 +1000

    graph transparency and auto-export, reordering stack values

--- workflow/scripts/finanalysis.R
+++ workflow/scripts/finanalysis.R
@@ -188,15 +188,15 @@ categorise_who_region <- function(country){
                             'Germany' = 'EUR', 'Ireland' = 'EUR', 'Belarus' = 'EUR',
                             'Russia' = 'EUR', 'Bulgaria' = 'EUR', 'Sweden' = 'EUR',
                             'Italy' = 'EUR', 'Slovenia' = 'EUR', 'France' = 'EUR',
-                            'Greece' = 'EUR', 'Iceland' = 'EUR', 'Brazil' = 'AMR',
+                            'Greece' = 'EUR', 'Iceland' = 'EUR', 'Brazil' = 'AMC',
                             'Japan' = 'WPR', 'China' = 'WPR', 'Guinea-Bissau' = 'AFR', 
-                            'India' = 'SEAR', 'Chile' = 'AMR', 'Tanzania' = 'AFR',
-                            'Armenia' = 'EUR', 'Ecuador' = 'AMR', 'Vietnam' = 'WPR',
+                            'India' = 'SEAR', 'Chile' = 'AMC', 'Tanzania' = 'AFR',
+                            'Armenia' = 'EUR', 'Ecuador' = 'AMC', 'Vietnam' = 'WPR',
                             'Philippines' = 'WPR', 'Korea' = 'WPR', 'Pakistan' = 'EMR', 
-                            'Gambia' = 'AFR', 'Caribbean' = 'AMR', 'USA' = 'AMR',
-                            'UK' = 'EUR', 'Suriname' = 'AMR', 'Angola' = 'AFR',
-                            'South Africa' = 'AFR', 'Argentina' = 'AMR', 'Thailand' = 'SEAR',
-                            'Singapore' = 'WPR', 'Canada' = 'AMR', 'Brazil' = 'AMR',
+                            'Gambia' = 'AFR', 'Caribbean' = 'AMC', 'USA' = 'AMC',
+                            'UK' = 'EUR', 'Suriname' = 'AMC', 'Angola' = 'AFR',
+                            'South Africa' = 'AFR', 'Argentina' = 'AMC', 'Thailand' = 'SEAR',
+                            'Singapore' = 'WPR', 'Canada' = 'AMC', 'Brazil' = 'AMC',
                             'Guinea' = 'AFR', 'Kenya' = 'AFR', 'Hong Kong' = 'WPR', 'Bhutan' = 'SEAR',
                             'Ivory Coast' = 'AFR', 'Malaysia' = 'WPR', 'Indonesia' = 'SEAR',
                             'Morocco' = 'EMR', 'Uganda' = 'AFR')
@@ -258,7 +258,7 @@ ggplot(dfplotamrbycountry,aes(fill=amrbycountryamr, y=amrbycountrycountnorm, x=a
   ggtitle('Relative frequency of Resistance genes found by Country') +
   xlab('Country') + ylab('Resistance Gene Frequency') +
   labs(color='Gene') + coord_flip() + theme_minimal() + 
-  scale_y_continuous(labels = scales::percent) + theme(legend.position = "none")
+  scale_y_continuous(labels = scales::percent) #+ theme(legend.position = "none")
 
 # create plot with continent sorting
 
@@ -266,12 +266,22 @@ ggplot(dfplotamrbycountry,aes(colour = amrbycountryamr, fill = amrbycountryamr, 
                              x = amrbycountrycountry)) + 
   geom_bar(position="stack", stat="identity",  width=.7) +
   xlab('Country') + ylab('Resistance Gene Frequency') + theme_classic() +
-  theme(legend.position = 'None')  + 
-  labs(color='Gene') +
+  #theme(legend.position = 'None')  + 
+  labs(colour='Gene', fill='Gene') +
   facet_grid(.~amrbycountrycontinent,scales = 'free',space = 'free',drop = F) + 
-  scale_y_continuous(labels = scales::percent) + guides(x =  guide_axis(angle = 45))
+  scale_y_continuous(labels = scales::percent) + guides(x =  guide_axis(angle = 45)) + 
+  theme(
+    panel.background = element_rect(fill='transparent'), #transparent panel bg
+    plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
+    panel.grid.major = element_blank(), #remove major gridlines
+    panel.grid.minor = element_blank(), #remove minor gridlines
+    legend.background = element_rect(fill='transparent'), #transparent legend bg
+    legend.box.background = element_rect(fill='transparent') #transparent legend panel
+  )
 
-# Cas gene by country plot ####
+ggsave('amrbycountry.svg', width=3240, height = 1944, units = 'px', bg = 'transparent')
+
+\# Cas gene by country plot ####
 # now create plot of cas genes by country with continent sorting
 {
   dfcasbycountry <- (matrix(0, nrow=(length(allcountries)),ncol=(length(allcasgenes))))
@@ -308,9 +318,19 @@ ggplot(dfplotcasbycountry,aes(colour = casbycountrycas, fill = casbycountrycas,
   geom_bar(position="stack", stat="identity",  width=.7) +
   xlab('Country') + ylab('Cas Gene Frequency') + theme_classic() +
   #theme(legend.position = 'None')  + 
-  labs(color='Gene') +
+  labs(colour='Gene', fill='Gene') +
   facet_grid(.~casbycountrycontinent,scales = 'free',space = 'free',drop = F) + 
-  scale_y_continuous(labels = scales::percent) + guides(x =  guide_axis(angle = 45))
+  scale_y_continuous(labels = scales::percent) + guides(x =  guide_axis(angle = 45)) + 
+  theme(
+    panel.background = element_rect(fill='transparent'), #transparent panel bg
+    plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
+    panel.grid.major = element_blank(), #remove major gridlines
+    panel.grid.minor = element_blank(), #remove minor gridlines
+    legend.background = element_rect(fill='transparent'), #transparent legend bg
+    legend.box.background = element_rect(fill='transparent') #transparent legend panel
+  )
+
+ggsave('casbycountry.svg', width=3240, height = 1944, units = 'px', bg = 'transparent')
 
 # AMR gene by sex network boxplot ####
 {
@@ -428,7 +448,7 @@ ggplot(dfplotcasbysexnorm,aes(fill=casbysexcas, y=casbysexnorm, x=casbysexsex)) 
   labs(color='Gene') + coord_flip() + theme_minimal() + 
   scale_y_continuous(labels = scales::percent) #+ theme(legend.position = 'None')
 
-# amr by cas gene heatmap
+# amr by cas gene heatmap ####
 {
   dfamrbycas <- data.frame(matrix(0,ncol=length(allcasgenes),nrow=length(allmechclasses)))
   rownames(dfamrbycas) <- allmechclasses
@@ -453,6 +473,21 @@ amrbycasdroplow <- as.matrix(dfamrbycasdroplow)
 heatmap(amrbycasdroplow, scale='row')
 heatmap(amrbycasdroplow, scale='column')
 heatmap(amrbycasdroplow, scale='none')
+
+library(RColorBrewer)
+library(svglite)
+coul <- colorRampPalette(brewer.pal(8, "BuPu"))(25)
+ht <- heatmap(amrbycas, scale='none', col = coul )
+svg("myplot.svg", width=600, height=400, bg = "transparent"); heatmap(amrbycas, scale='none', col = coul ); dev.off()
+
+ggsave('myplot.svg', width=600, height=400, units = 'px',  bg='transparent')
+
+svglite('myplot.svg', width=12, height=8, bg='transparent')
+heatmap(amrbycas, scale='none', col = coul )
+dev.off()
+svglite('amrbycasdroplow.svg', width=6, height=4, bg='transparent')
+heatmap(amrbycasdroplow, scale='none', col = coul )
+dev.off()
 
 # world map jittered points resistance ####
 library(rgeos)
@@ -525,7 +560,220 @@ ggplot(dfplotmapamrbycountry) + geom_map(dat = world_map, map = world_map, aes(m
   expand_limits(x = world_map$long, y = world_map$lat) + 
   geom_jitter(aes(x = amrx, y = amry, colour = amrbycountryamr, alpha=amrbycountrycount), height = 10 , width = 10 )
 
-ggplot(dfplotmapamrbycountry) + 
-  geom_map(dat = world_map, map = world_map, aes(map_id = region), fill = 'white', 
-           color = '#7f7f7f', size = 0.25) + expand_limits(x = world_map$long, y = world_map$lat) +
-  geom_point(aes(x=amrx,y=amry)) + geom_jitter(aes(x=amrx,y=amry), colour = amrbycountryamr, width = 0.5, height = 0.5)
+# heatmap of amr x cas without categorising amr genes ####
+all_amr_list <- c()
+for(r in 1:nrow(dframesex)) {
+  all_amr_list <- unique(c(all_amr_list, unlist(strsplit(dframesex$AMR.Gene.symbol[r], ','))))
+}
+
+{
+  dfamrbyuncat <- data.frame(matrix(0,ncol=length(allcasgenes),nrow=length(all_amr_list)))
+  rownames(dfamrbyuncat) <- all_amr_list
+  colnames(dfamrbyuncat) <- allcasgenes
+  for(r in 1:nrow(dframesex)) {
+    if(dframesex$AMR.Gene.symbol[r] == '' ) {
+      next
+    }
+    amr_list <- unlist(strsplit(dframesex$AMR.Gene.symbol[r], ','))
+    cas_list <- categorise_cas((dframesex$Genes[r]))
+    print(r)
+    for(a in 1:length(amr_list)) {
+      for(c in 1:length(cas_list)) {
+        dfamrbyuncat[amr_list[a],cas_list[c]] <- dfamrbyuncat[amr_list[a],cas_list[c]] + 1
+      }
+    }
+  }
+  amrbyuncat <- as.matrix(dfamrbyuncat)
+}
+heatmap(amrbyuncat, scale='row')
+heatmap(amrbyuncat, scale='column')
+heatmap(amrbyuncat, scale='none')
+
+# heatmap of cas genes by sex network ####
+
+heatmap(dfcasbysex, scale='row')
+heatmap(dfcasbysex, scale='column')
+heatmap(dfcasbysex, scale='none')
+
+svglite('amrbyuncat.svg', width=16, height=10, bg='transparent')
+heatmap(amrbyuncat, scale='none', col = coul )
+dev.off()
+
+# stacked bar graph of cas by country without normalisation
+casbycountrycount <- c()
+for(r in 1:nrow(dfcasbycountry)) {
+  for(c in 1:ncol(dfcasbycountry)) {
+    casbycountrycount <- c(casbycountrycount, (dfcasbycountry[r,c]))
+  }
+}
+dfplotcasbycountryunnorm <- data.frame(casbycountrycount,casbycountrycas,casbycountrycountry,casbycountrycontinent)
+ggplot(dfplotcasbycountryunnorm,aes(colour = casbycountrycas, fill = casbycountrycas, 
+                              y = casbycountrycount, x = casbycountrycountry)) + 
+  geom_bar(position="stack", stat="identity",  width=.7) +
+  xlab('Country') + ylab('Cas Gene Frequency') + theme_classic() +
+  theme(legend.position = 'None')  + 
+  labs(fill='Gene', colour='Gene') +
+  facet_grid(.~casbycountrycontinent,scales = 'free',space = 'free',drop = F) + guides(x =  guide_axis(angle = 45))
+
+# create country plots with n= values over them ####
+{
+  countrycount <- data.frame(matrix(0,nrow=length(allcountries),ncol=1))
+rownames(countrycount) <- allcountries
+colnames(countrycount) <- c('Count')
+sexcount <- data.frame(matrix(0,nrow=length(allsexnetworks),ncol=1))
+rownames(sexcount) <- allsexnetworks
+colnames(sexcount) <- c('Count')
+countrycountstring <- c()
+sexcountstring <- c()
+for(r in 1:nrow(dframesex)) {
+  if(!(dframesex$Country[r] == '' )) {
+    countrycount[dframesex$Country[r],'Count'] <- 
+      countrycount[dframesex$Country[r],'Count'] + 1
+  }
+  if(!(dframesex$Sexual.behaviour[r] == '' )) {
+    sexcount[dframesex$Sexual.behaviour[r],'Count'] <-
+      sexcount[dframesex$Sexual.behaviour[r],'Count'] + 1
+  } 
+}
+for(n in 1:length(amrbycountrycountry)) {
+  countrycountstring <- 
+    c(countrycountstring, paste0('n = ', countrycount[amrbycountrycountry[n],'Count']))
+}
+dfplotamrbycountryn <- data.frame(amrbycountrycountnorm,amrbycountryamr,amrbycountrycountry,amrbycountrycontinent,countrycountstring)
+}
+ggplot(dfplotamrbycountryn,aes(colour = amrbycountryamr, fill = amrbycountryamr, y = amrbycountrycountnorm,
+                              x = amrbycountrycountry)) + 
+  geom_bar(position="stack", stat="identity",  width=.7) +
+  xlab('Country') + ylab('Resistance Gene Frequency') + theme_classic() +
+  #theme(legend.position = 'None')  + 
+  labs(colour='Gene',fill='Gene') + geom_text(data = dfplotamrbycountryn, aes(label = countrycountstring, colour = 'black')) +
+  facet_grid(.~amrbycountrycontinent,scales = 'free',space = 'free',drop = F) + 
+  scale_y_continuous(labels = scales::percent) + guides(x =  guide_axis(angle = 45))
+
+# copy to clipboard mac ####
+write.table(sexcount, file=pipe('pbcopy'), sep='\t')
+
+tempcontinent <- c()
+for(r in 1:nrow(dframesex)) {
+  tempcountry <- dframesex$Country[r]
+  tempcontinent <- c(tempcontinent, categorise_who_region(tempcountry))
+  if(categorise_who_region(tempcountry) == 'EMR' ) {print(tempcountry)}
+}
+countinent <- table(tempcontinent)
+countinent
+
+function(cas_list) {
+  return_list <- c()
+  if(!(cas_list == '')) {
+    cas_list <- unlist(strsplit(cas_list,','))
+    for(n in 1:length(cas_list)) {
+      return_list <- unique(c(return_list,strsplit(cas_list[n],'_')[[1]][1]))
+    }
+  }
+  return(return_list)
+}
+
+caspercent <- data.frame(matrix(0,nrow=1,ncol=5), row.names = c('Count'))
+colnames(caspercent) <- c('No cas genes count','Cas4, Cas7, Cas8c count', 'Cas1 count', 'DEDDH count','Total count')
+for(r in 1:nrow(dframesex)) {
+  caspercent[1,5] <- caspercent[1,5] + 1
+  if(dframesex$Genes[r] == '' ) {
+    caspercent[1,1] <- caspercent[1,1] + 1
+  }else {
+    cas_list <- categorise_cas(dframesex$Genes[r])
+    if( ('Cas4' %in% cas_list) && ('Cas7' %in% cas_list) && ('Cas8c' %in% cas_list) ) {
+      caspercent[1,2] <- caspercent[1,2] + 1
+    }
+    if('Cas1' %in% cas_list) {
+      caspercent[1,3] <- caspercent[1,3] + 1
+    }
+    if('DEDDh' %in% cas_list) {
+      caspercent[1,4] <- caspercent[1,4] + 1
+    }
+  }
+}
+dframesex$Genes[130L]
+length(dframesex$Genes[130L])
+
+triadpercent <- caspercent[1,2] / caspercent[1,5]
+cas1percent <- caspercent[1,3] / caspercent[1,5]
+deddpercent <- caspercent[1,4] / caspercent[1,5]
+
+triadpercent 
+cas1percent 
+deddpercent 
+
+countrieswithdeddh <- c()
+for(r in 1:nrow(dframesex)) {
+  cas_list <- categorise_cas(dframesex$Genes[r])
+  if('DEDDh' %in% cas_list) {
+    countrieswithdeddh <- c(countrieswithdeddh,dframesex$Country[r])
+  }
+  countrieswithdeddh <- unique(countrieswithdeddh)
+}
+deddhframe <- data.frame(matrix(0,nrow=length(countrieswithdeddh),ncol=1))
+rownames(deddhframe) <- countrieswithdeddh
+for(r in 1:nrow(dframesex)) {
+  cas_list <- categorise_cas(dframesex$Genes[r])
+  if(!(dframesex$Country[r] == '' )) {
+    if('DEDDh' %in% cas_list) {
+      deddhframe[dframesex$Country[r],1] <- deddhframe[dframesex$Country[r],1] + 1
+    }
+  }
+}
+colnames(deddhframe) <- c('Count')
+deddhfram <- deddhframe[-3,]
+deddhframe[3,]
+deddhfram
+
+#reordering test ####
+d <- data.frame(
+  y=c(0.1, 0.2, 0.7),
+  cat = factor(c('No', 'Yes', 'NA'), levels = c('NA', 'Yes', 'No')))
+
+p1 <- ggplot(d, aes(x=1, y=y, fill=cat)) +
+  geom_bar(stat='identity')
+
+# Change order of rows
+p2 <- ggplot(d[c(2, 3, 1), ], aes(x=1, y=y, fill=cat)) +
+  geom_bar(stat='identity')
+
+# Change order of levels
+d$cat2 <- relevel(d$cat, 'Yes')
+d$cat2 <- relevel(d$cat, 'No')
+p3 <- ggplot(d, aes(x=1, y=y, fill=cat2)) +
+  geom_bar(stat='identity') 
+
+grid.arrange(p1, p2, p3, ncol=3)
+
+ggplot(d, aes(x=1, y=y, fill=cat)) +
+  geom_bar(stat='identity')
+
+# get original levels
+levels(as.factor(dfplotamrbycountryreorder$amrbycountryamr))
+# create vector to take new levels
+newlevels <- levels(as.factor(dfplotamrbycountry$amrbycountryamr))
+# create vector length of original vector and permute however you want
+map <- c(16,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,17,18,19,20,21,22,23,24,25)
+# now new levels vector is ordered by that map
+newlevels <- newlevels[order(map)]
+# in ggplot replace fill and or colour = x with = factor(x,levels=newlevels)
+
+ggplot(dfplotamrbycountryreorder,aes(colour = factor(amrbycountryamr, levels=newlevels), fill = factor(amrbycountryamr, levels=newlevels), y = amrbycountrycountnorm,
+                              x = amrbycountrycountry)) + 
+  geom_bar(position="stack", stat="identity",  width=.7) +
+  xlab('Country') + ylab('Resistance Gene Frequency') + theme_classic() +
+  #theme(legend.position = 'None')  + 
+  labs(colour='Gene', fill='Gene') +
+  facet_grid(.~amrbycountrycontinent,scales = 'free',space = 'free',drop = F) + 
+  scale_y_continuous(labels = scales::percent) + guides(x =  guide_axis(angle = 45)) + 
+  theme(
+    panel.background = element_rect(fill='transparent'), #transparent panel bg
+    plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
+    panel.grid.major = element_blank(), #remove major gridlines
+    panel.grid.minor = element_blank(), #remove minor gridlines
+    legend.background = element_rect(fill='transparent'), #transparent legend bg
+    legend.box.background = element_rect(fill='transparent') #transparent legend panel
+  )
+
+ggsave('amrbycountry.svg', width=3240, height = 1944, units = 'px', bg = 'transparent')
