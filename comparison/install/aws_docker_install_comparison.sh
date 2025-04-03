5b4b341cf017c222ba9e773fa4cd0fb0c906fa33
AvCloudy <avcloudy@gmail.com>
2025-04-04 00:50:18 +1000

    Update aws_docker_install_comparison.sh

--- comparison/install/aws_docker_install_comparison.sh
+++ comparison/install/aws_docker_install_comparison.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+# install docker instances
+# this needs to be run as root, which is why it's a seperate script
+# as part of a pipeline, this might need to be a requirement and not installed by us
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# create docker group so can run docker without elevated privileges 
+groupadd docker
+usermod -aG docker $USER
+
+# need to restart at this point
