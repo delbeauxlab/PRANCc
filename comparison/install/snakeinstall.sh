1a092881f76e08e17f2d863663da2193f1c229b1
avcloudy <avcloudy@gmail.com>
2025-05-29 17:29:22 +1000

    Update snakeinstall.sh

--- comparison/install/snakeinstall.sh
+++ comparison/install/snakeinstall.sh
@@ -11,14 +11,15 @@
 
 pip install gdown
 
+mkdir prancc/bin
 mkdir prancc/bin/ccfinder
 mkdir prancc/bin/cidentify
 curl -LOZ "https://github.com/dcouvin/CRISPRCasFinder/archive/refs/tags/release-4.3.2.tar.gz" \
  "https://github.com/BackofenLab/CRISPRidentify/archive/refs/tags/v1.2.1.tar.gz" \
  "https://github.com/BackofenLab/CRISPRcasIdentifier/archive/refs/tags/v1.1.0.tar.gz"
 
-gdown https://drive.google.com/file/d/1YbTxkn9KuJP2D7U1-6kL1Yimu_4RqSl1/view?usp=sharing
-gdown https://drive.google.com/file/d/1Nc5o6QVB6QxMxpQjmLQcbwQwkRLk-thM/view?usp=sharing
+gdown https://drive.google.com/file/d/1YbTxkn9KuJP2D7U1-6kL1Yimu_4RqSl1
+gdown https://drive.google.com/file/d/1Nc5o6QVB6QxMxpQjmLQcbwQwkRLk-thM
 
 tar -xvzf release-4.3.2.tar.gz -C prancc/bin/ccfinder --strip-components=1
 tar -xzvf v1.2.1.tar.gz -C prancc/bin/cidentify --strip-components=1
@@ -26,6 +27,7 @@ tar -xzvf v1.1.0.tar.gz -C prancc/bin/cidentify/tools/CRISPRcasIdentifier/CRISPR
 mv -t prancc/bin/cidentify/tools/CRISPRcasIdentifier/CRISPRcasIdentifier HMM_sets.tar.gz trained_models.tar.gz
 rm release-4.3.2.tar.gz v1.2.1.tar.gz v1.1.0.tar.gz
 
+conda init
 # create a new environment based on the ccf.environment.yml file
 conda env create -f prancc/bin/ccf/ccf.environment.yml -n crisprcasfinder
 
