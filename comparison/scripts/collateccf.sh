c47db4d38174e7f557c2a847709158b0dd4bc6fb
cloudy <avcloudy@gmail.com>
2025-06-17 19:05:51 +1000

    double send header removed

--- comparison/scripts/collateccf.sh
+++ comparison/scripts/collateccf.sh
@@ -12,27 +12,6 @@ output_file_crispr=${3:-"results/ccfcrispr.tsv"}
 > "$output_file_cas"
 > "$output_file_crispr"
 
-# Add headers to the TSV files
-cas_header="Filename\tSequence(s)\tCRISPR array(s)\tNb CRISPRs\tEvidence-levels\tCas cluster(s)\t"
-cas_header+="Nb Cas\tCas Types/Subtypes"
-crispr_header="Filename\tStrain	Sequence\tSequence_basename\tDuplicated_Spacers\tCRISPR_Id\t"
-crispr_header+="CRISPR_Start\tCRISPR_End\tCRISPR_Length\tPotential_Orientation (AT%)\t"
-crispr_header+="CRISPRDirection\tConsensus_Repeat\tRepeat_ID (CRISPRdb)\t"
-crispr_header+="Nb_CRISPRs_with_same_Repeat (CRISPRdb)\tRepeat_Length\tSpacers_Nb\tMean_size_Spacers\t"
-crispr_header+="Standard_Deviation_Spacers\tNb_Repeats_matching_Consensus\tRatio_Repeats_match/TotalRepeatt"
-crispr_header+="Conservation_Repeats (% identity)\tEBcons_Repeats\tConservation_Spacers (% identity)\t"
-crispr_header+="EBcons_Spacers\tRepeat_Length_plus_mean_size_Spacers\tRatio_Repeat/mean_Spacers_Length\t"
-crispr_header+="CRISPR_found_in_DB (if sequence IDs are similar)\tEvidence_Level\t"
-
-echo -e $cas_header >> $output_file_cas
-echo -e $crispr_header >> $output_file_crispr
-
-# Create variables to keep track of total sequences processed, the number of spacers 
-# and the number of cas systems (for an average)
-total_sequences=0
-total_spacers=0
-total_cas=0
-
 # loop over results summaries in TSV folder per output sequence
 # add on the fna fikes name and collate
 counter=0
@@ -57,15 +36,4 @@ do
         echo -en "$foldername\t" >> $output_file_crispr
         echo -e "$line" >> $output_file_crispr
     done < $folder/TSV/Crisprs_REPORT.tsv
-
-    # sed 1d $folder/TSV/CRISPR-Cas_summary.tsv | while read line
-    # do
-    #     echo -en "$foldername\t" >> $output_file_cas
-    #     echo -e $line >> $output_file_cas
-    # done
-    # sed 1d $folder/TSV/Crisprs_REPORT.tsv | while read line
-    # do
-    #     echo -en "$foldername\t" >> $output_file_crispr
-    #     echo -e $line >> $output_file_crispr
-    # done
 done
