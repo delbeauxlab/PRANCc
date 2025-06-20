7044220d07432c05c1252ae574faf79e85080459
cloudy <avcloudy@gmail.com>
2025-06-20 22:44:33 +1000

    update for 16 threads, latest miniforge version

--- comparison/install/awsinstall.sh
+++ comparison/install/awsinstall.sh
@@ -4,10 +4,10 @@ apt-get update
 apt-get install build-essential
 apt-get unzip
 
-curl -L -O "https://github.com/conda-forge/miniforge/releases/download/25.1.1-2/Miniforge3-25.1.1-2-Linux-x86_64.sh"
+curl -L -O "https://github.com/conda-forge/miniforge/releases/download/25.3.0-3/Miniforge3-25.3.0-3-Linux-x86_64.sh"
 # run install script in non-interactive mode
-./Miniforge3-25.1.1-2-Linux-x86_64.sh -b
-rm Miniforge3-25.1.1-2-Linux-x86_64.sh
+./Miniforge3-25.3.0-3-Linux-x86_64.sh -b
+rm Miniforge3-25.3.0-3-Linux-x86_64.sh
 
 eval "$(conda shell.bash activate)"
 conda create -n snakemake -c conda-forge -c bioconda snakemake
