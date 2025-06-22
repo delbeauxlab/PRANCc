0ec7334d89e26b5225b4b7f3322b47299f60f446
cloudy <avcloudy@gmail.com>
2025-06-22 11:35:58 +1000

    remove crispr_identify_env log write

--- comparison/install/awsinstall.sh
+++ comparison/install/awsinstall.sh
@@ -2,14 +2,15 @@
 
 apt-get update
 apt-get install build-essential
-apt-get unzip
+apt install unzip
 
 curl -L -O "https://github.com/conda-forge/miniforge/releases/download/25.3.0-3/Miniforge3-25.3.0-3-Linux-x86_64.sh"
 # run install script in non-interactive mode
+chmod +x Miniforge3-25.3.0-3-Linux-x86_64.sh
 ./Miniforge3-25.3.0-3-Linux-x86_64.sh -b
 rm Miniforge3-25.3.0-3-Linux-x86_64.sh
 
-eval "$(conda shell.bash activate)"
-conda create -n snakemake -c conda-forge -c bioconda snakemake
+miniforge3/bin/conda init bash
+miniforge3/bin/conda create -n snakemake -c conda-forge -c bioconda snakemake
 
 
