9bc2912ed69e8d0efe257eff5aac420b81d58519
cloudy <avcloudy@gmail.com>
2025-07-06 23:17:05 +1000

    fixes to install scripts
    fixes to benchmark assignation logic in
    rule cat_benchmarks
    final version push to main

--- workflow/install/awsinstall.sh
+++ workflow/install/awsinstall.sh
@@ -9,18 +9,19 @@
 # because of apt-get calls, needs to be run as superuser
 ###########
 
-apt-get update
-apt-get install build-essential
-apt install unzip
+# run these with sudo
+# apt-get update
+# apt-get install build-essential
+# apt install unzip
 
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 # run install script in non-interactive mode
 
-bash Miniforge3-$(uname)-$(uname -m).sh -b -p "/home/${SUDO_USER}/conda"
+bash Miniforge3-$(uname)-$(uname -m).sh -b -p "${HOME}/conda"
 rm Miniforge3-$(uname)-$(uname -m).sh
-source "/home/${SUDO_USER}/conda/etc/profile.d/conda.sh"
+source "${HOME}/conda/etc/profile.d/conda.sh"
 # For mamba support also run the following command
-source "/home/${SUDO_USER}/conda/etc/profile.d/mamba.sh"
+source "${HOME}/conda/etc/profile.d/mamba.sh"
 
 conda activate
 conda create -n snakemake -c conda-forge -c bioconda snakemake -y
--- workflow/install/install_prancc.sh
+++ workflow/install/install_prancc.sh
@@ -17,17 +17,24 @@
 # runs crisprcasidentifier to initialise it for crispridentify, sends output to /dev/null
 ###########
 
-pip install gdown
 
-mkdir prancc/bin
-mkdir prancc/bin/ccfinder
-mkdir prancc/bin/cidentify
+source "${HOME}/conda/etc/profile.d/conda.sh"
+# For mamba support also run the following command
+source "${HOME}/conda/etc/profile.d/mamba.sh"
+
+conda create -n gdown -c conda-forge gdown -y
+conda activate gdown
+gdown --fuzzy https://drive.google.com/file/d/1YbTxkn9KuJP2D7U1-6kL1Yimu_4RqSl1/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/1Nc5o6QVB6QxMxpQjmLQcbwQwkRLk-thM/view?usp=sharing
+conda deactivate
+conda remove -n gdown --all -y
+
+mkdir -p prancc/bin
+mkdir -p prancc/bin/ccfinder
+mkdir -p prancc/bin/cidentify
 curl -LO "https://github.com/dcouvin/CRISPRCasFinder/archive/refs/tags/release-4.3.2.tar.gz"
 curl -LO "https://github.com/BackofenLab/CRISPRidentify/archive/refs/tags/v1.2.1.tar.gz"
 curl -LO "https://github.com/BackofenLab/CRISPRcasIdentifier/archive/refs/tags/v1.1.0.tar.gz"
-
-gdown --fuzzy https://drive.google.com/file/d/1YbTxkn9KuJP2D7U1-6kL1Yimu_4RqSl1/view?usp=sharing
-gdown --fuzzy https://drive.google.com/file/d/1Nc5o6QVB6QxMxpQjmLQcbwQwkRLk-thM/view?usp=sharing
 
 tar -xvzf release-4.3.2.tar.gz -C prancc/bin/ccfinder --strip-components=1
 tar -xzvf v1.2.1.tar.gz -C prancc/bin/cidentify --strip-components=1
@@ -40,7 +47,7 @@ conda env create -f prancc/bin/ccfinder/ccf.environment.yml -n crisprcasfinder -
 
 conda activate crisprcasfinder
 # using mamba, install macysyfinder 2.1.2
-mamba install -c bioconda macsyfinder=2.1.2
+mamba install -c bioconda macsyfinder=2.1.2 -y
 # using macsyfinder, install CASFinder
 macsydata install -u CASFinder==3.1.0
 
@@ -56,9 +63,10 @@ install-crisprdetect
 
 conda deactivate
 
-conda create -f prancc/bin/cidentify/environment.yml -n crispridentifytemp -y
+conda env create -f prancc/bin/cidentify/environment.yml -y
 cd prancc/bin/cidentify/tools/CRISPRcasIdentifier/CRISPRcasIdentifier/
-conda activate crispridentifytemp
-python CRISPRcasIdentifier.py -f ../../../../../test/dummy.fasta -st dna -sc complete -o /dev/null
+conda activate crispr_identify_env
+python CRISPRcasIdentifier.py -f ../../../../../upload/GCF_040370845.1_WHO_U_2024_genomic.fasta -st dna -sc complete -o /dev/null
 cd ../../../../../..
-conda remove -n crispridentifytemp --all
+conda deactivate
+conda remove -n crispr_identify_env --all -y
