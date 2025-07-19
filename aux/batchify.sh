be941b43543e535f242ce8133d4a94f3aff9dc12
cloudy <avcloudy@gmail.com>
2025-07-20 02:37:27 +1000

    version number update
    batching scripts changed to append directly to tarball instead of creating
    intermediate folders
    
    basic config.yaml structure for running files in input dir vs from list
    vs from accession number list

--- aux/batchify.sh
+++ aux/batchify.sh
@@ -2,37 +2,34 @@
 
 ##########
 # batchify.sh
-# ./batchify.sh [INPUT_DIR] [NUM_BATCHES]
-# takes a single directory as input and generates a number of output directories each
-# containing [NUM_BATCHES] fastas in [INPUT_DIR] and a list of the files in [INPUT_DIR]
-# and what batch folder they were copied to
+# ./batchify.sh [INPUT_DIR] [BATCH_SIZE]
+# takes a single directory as input and generates a number of output tarballs each
+# containing less than or equal to [BATCH_SIZE] fastas in [INPUT_DIR] and a list of the 
+# files in [INPUT_DIR] and what batch tarball they were added to
+# then compresses all tarballs in [INPUT_DIR] with gzip
 #
 # uses bsd tar from a mac, hence removing mac-specific headers
 #########
 
 input_dir=${1}
 
-num_batches=${2}
+batch_size=${2:-500}
+
+cd $input_dir
+num_fastas=$(ls -1 *.fasta 2>/dev/null | wc -l)
+
+num_batches=$(( ($num_fastas + $batch_size - 1) / $batch_size ))
 
 counter=0
+cd $input_dir
 echo -e "Filename\tBatch" > $input_dir/batches.tsv
-for number in $(seq 1 $num_batches)
-    do
-        value=$(printf "%02d" $number)
-        mkdir -p $input_dir/$value
-    done
 for fasta in $input_dir/*.fasta
 do
-    value=$(printf "%02d" $(($counter % $num_batches + 1)))
-    cp $fasta $input_dir/$value
+    value=$(printf "%02d" $(( ($counter % $num_batches) + 1)))
     echo -e $(basename $fasta) '\t' $value >> $input_dir/batches.tsv
+    tar --disable-copyfile --no-xattrs -rvf $value.tar $fasta
     counter=$(($counter+1))
 done
-for number in $(seq 1 $num_batches)
-do
-    value=$(printf "%02d" $number)
-    cd $input_dir/$value
-    tar --disable-copyfile --no-xattrs -czvf $input_dir/$value.tar.gz *.fasta
-    rm -r $input_dir/$value
-done
-cd
+
+echo "gzip -v" *.tar
+gzip -v *.tar
---
+++ aux/rebatchify.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##########
+# rebatchify.sh
+# ./rebatchify.sh [INPUT_DIR] [BATCH_FILE]
+# takes a directory of fastas and a [BATCH_FILE] such as created from batchify.sh and 
+# recreates those tarballs then compresses tarballs in folder with gzip
+#
+# uses bsd tar from a mac, hence removing mac-specific headers
+#########
+
+input_dir=${1}
+
+batch_file=${2:-"$input_dir/batches.tsv"}
+
+cd $input_dir
+
+while read filename batch
+do
+    tar -rvf $batch $filename
+done < <(sed 1d $batch_file)
+
+echo "gzip -v " *.tar
+gzip -v *.tar
